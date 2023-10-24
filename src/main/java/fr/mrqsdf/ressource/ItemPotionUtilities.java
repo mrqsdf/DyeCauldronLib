813803c987620ac61417f508c1c5c12735201aa0
BuildTools <unconfigured@null.spigotmc.org>
2023-10-24 16:53:13 +0200

    add potion data item

---
+++ src/main/java/fr/mrqsdf/ressource/ItemPotionUtilities.java
@@ -0,0 +1,61 @@
+package fr.mrqsdf.ressource;
+
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.w3c.dom.css.RGBColor;
+
+public class ItemPotionUtilities {
+
+    public static ItemStack dyeCauldronLevel1(int rgb){
+        ItemStack item = new ItemStack(Material.POTION);
+        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+        potionMeta.setColor(Color.fromRGB(rgb));
+        potionMeta.setCustomModelData(531001);
+        item.setItemMeta(potionMeta);
+        return item;
+    }
+    public static ItemStack dyeCauldronLevel2(int rgb){
+        ItemStack item = new ItemStack(Material.POTION);
+        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+        potionMeta.setColor(Color.fromRGB(rgb));
+        potionMeta.setCustomModelData(531002);
+        item.setItemMeta(potionMeta);
+        return item;
+    }
+    public static ItemStack dyeCauldronLevel3(int rgb){
+        ItemStack item = new ItemStack(Material.POTION);
+        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+        potionMeta.setColor(Color.fromRGB(rgb));
+        potionMeta.setCustomModelData(531003);
+        item.setItemMeta(potionMeta);
+        return item;
+    }
+
+    public static ItemStack dyeCauldronLeve1(int r, int g, int b){
+        ItemStack item = new ItemStack(Material.POTION);
+        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+        potionMeta.setColor(Color.fromRGB(r, g, b));
+        potionMeta.setCustomModelData(531001);
+        item.setItemMeta(potionMeta);
+        return item;
+    }
+    public static ItemStack dyeCauldronLevel2(int r, int g, int b){
+        ItemStack item = new ItemStack(Material.POTION);
+        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+        potionMeta.setColor(Color.fromRGB(r, g, b));
+        potionMeta.setCustomModelData(531002);
+        item.setItemMeta(potionMeta);
+        return item;
+    }
+    public static ItemStack dyeCauldronLevel3(int r, int g, int b){
+        ItemStack item = new ItemStack(Material.POTION);
+        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+        potionMeta.setColor(Color.fromRGB(r, g, b));
+        potionMeta.setCustomModelData(531003);
+        item.setItemMeta(potionMeta);
+        return item;
+    }
+
+}
