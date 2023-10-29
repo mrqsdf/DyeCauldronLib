d5b010a9c8299d1011fdd5643fe90e39eb4378c9
BuildTools <unconfigured@null.spigotmc.org>
2023-10-29 17:02:00 +0100

    refactor file

--- src/main/java/fr/mrqsdf/dyecauldron/listener/CauldronInteractListener.java
+++ src/main/java/fr/mrqsdf/dyecauldron/listener/CauldronInteractListener.java
@@ -7,7 +7,9 @@ import jdk.jfr.Description;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
+import org.bukkit.block.TileState;
 import org.bukkit.block.data.Levelled;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
@@ -19,6 +21,7 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.persistence.PersistentDataType;
 
 public class CauldronInteractListener implements Listener {
 
@@ -38,6 +41,7 @@ public class CauldronInteractListener implements Listener {
         if (!DyeColorUtils.dyeMaterial.contains(item.getType())) return;
         if (block.getType() == Material.CAULDRON || block.getType() == Material.LAVA_CAULDRON || block.getType() == Material.POWDER_SNOW_CAULDRON) return;
         Levelled cauldron = (Levelled) block.getBlockData();
+        TileState tileState = (TileState) block.getState();
         int lvlData = cauldron.getLevel();
         block.setType(Material.CAULDRON);
         if (cauldron.getLevel() == 0) return;
@@ -46,9 +50,10 @@ public class CauldronInteractListener implements Listener {
         location.setZ(location.getZ() + 0.5);
         ArmorStand armorStand = SummonArmorstand.summon(location, DyeColorUtils.dyeColor.get(item.getType())[0],
                 DyeColorUtils.dyeColor.get(item.getType())[1],DyeColorUtils.dyeColor.get(item.getType())[2], lvlData);
-        block.setMetadata("armorstand", new FixedMetadataValue(DyeCauldron.plugin, armorStand));
-        block.setMetadata("color", new FixedMetadataValue(DyeCauldron.plugin, DyeColorUtils.dyeColor.get(item.getType())));
-        block.setMetadata("level", new FixedMetadataValue(DyeCauldron.plugin, lvlData));
+        tileState.getPersistentDataContainer().set(new NamespacedKey(DyeCauldron.plugin, "armorstand"), PersistentDataType.STRING, armorStand.getUniqueId().toString());
+        tileState.getPersistentDataContainer().set(new NamespacedKey(DyeCauldron.plugin, "color"), PersistentDataType.INTEGER_ARRAY,DyeColorUtils.dyeColor.get(item.getType()));
+        tileState.getPersistentDataContainer().set(new NamespacedKey(DyeCauldron.plugin, "level"), PersistentDataType.INTEGER, lvlData);
+        tileState.update();
     }
 
     @Description(
